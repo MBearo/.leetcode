2f9b8f4085f06b1286307e3f56fbfad434aad43b
MBearo <295290708@qq.com>
2020-07-23 19:06:40 +0800

    UPG

--- 26.删除排序数组中的重复项.js
+++ 26.删除排序数组中的重复项.js
@@ -7,15 +7,31 @@
  * @param {number[]} nums
  * @return {number}
  */
-// 提上说 前 几个元素修改为xxxx，可以不用改所有的
+// 题上说 前 几个元素修改为xxxx，可以不用改所有的
 var removeDuplicates = function (nums) {
-  for (let i = 0; i < nums.length; ) {
-    if (nums[i] === nums[i + 1]) {
-      nums.splice(i, 1)
+  // for (let i = 0; i < nums.length; ) {
+  //   if (nums[i] === nums[i + 1]) {
+  //     nums.splice(i, 1)
+  //   } else {
+  //     i++
+  //   }
+  // }
+  //return nums.length
+
+  // 快慢指针
+  let slowP = 0
+  let fastP = 0
+  while (fastP < nums.length) {
+    if (nums[fastP] === nums[slowP]) {
+      fastP++
     } else {
-      i++
+      slowP++
+      if (fastP !== slowP) {
+        nums[slowP] = nums[fastP]
+      }
+      fastP++
     }
   }
-  return nums.length
+  return slowP + 1
 };
 
