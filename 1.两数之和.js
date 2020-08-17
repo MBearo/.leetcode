da4205d95fedb7293b67132c2b6741b6956c07b4
MBearo <295290708@qq.com>
2020-08-17 09:59:29 +0800

    UPG:1

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -8,15 +8,26 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  let arr=[]
-  nums.forEach((value,index)=>{
-    for (let i = index+1; i < nums.length; i++) {
-      if(arr.length!==0){
+var twoSum = function (nums, target) {
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    if (map.has(target - element)) {
+      return [i, map.get(target - element)]
+    } else {
+      map.set(element, i)
+    }
+  }
+}
+var twoSum2 = function (nums, target) {
+  let arr = []
+  nums.forEach((value, index) => {
+    for (let i = index + 1; i < nums.length; i++) {
+      if (arr.length !== 0) {
         break
       }
-      if(value+nums[i]===target){
-        arr.push(index,i)
+      if (value + nums[i] === target) {
+        arr.push(index, i)
         break
       }
     }
@@ -24,3 +35,4 @@ var twoSum = function(nums, target) {
   return arr
 };
 
+console.log(twoSum([2, 7, 11, 15], 9));
