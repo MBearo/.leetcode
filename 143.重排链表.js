b9f362cc192cbd8489cce04f65f3d21dbd8ee61a
MBearo <295290708@qq.com>
2020-08-31 15:54:26 +0800

    UPG:143

--- 143.重排链表.js
+++ 143.重排链表.js
@@ -16,37 +16,43 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
+// 快慢指针找中点，翻转后半部分，依次拼接
 var reorderList = function (head) {
-  let copy = null
-  let current = head
-  while (current) {
-    const node = new ListNode(current.val, copy)
-    copy = node
-    current = current.next
+  if (head === null || head.next === null || head.next.next === null) return head
+  let slow = head
+  let fast = head
+  while (fast.next !== null && fast.next.next !== null) {
+    slow = slow.next
+    fast = fast.next.next
   }
+  let p = slow.next
+
+  slow.next = null
+  console.log(p);
+  // 把p翻转一下
+  let rp = reverseList(p)
   let cur = head
-  let cur2 = copy
-  while (true) {
-    console.log(cur, cur2);
-    if (cur && cur2) {
-      if (cur.val === cur2.val) {
-        cur.next = null
-        break
-      } else if (cur.val === cur2.next.val) {
-        cur.next.next = null
-        break
-      }
-      const nxt = cur2.next
-      cur2.next = cur.next
-      cur.next = cur2
-      cur = cur.next.next
-      cur2 = nxt
-    } else {
-      break
-    }
+  while (rp) {
+    const nxt = cur.next
+    const rxt = rp.next
+    cur.next = rp
+    rp.next = nxt
+    rp = rxt
+    cur = nxt
   }
   return head
 };
+function reverseList(head) {
+  let cur = head
+  let pre = null
+  while (cur) {
+    const next = cur.next
+    cur.next = pre
+    pre = cur
+    cur = next
+  }
+  return pre
+}
 // @lc code=end
 function ListNode(val, next) {
   this.val = val;
@@ -55,7 +61,7 @@ function ListNode(val, next) {
 var list = create(1)
 
 function create(val) {
-  if (val <= 5) {
+  if (val <= 4) {
     const node = new ListNode(val)
     node.next = create(val + 1)
     return node
@@ -63,4 +69,4 @@ function create(val) {
   return null
 }
 //console.log(list);
-//console.log(JSON.stringify(reorderList(list)))
+console.log(JSON.stringify(reorderList(list)))
