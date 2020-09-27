911c84817858bd9bb1a9e1220e3c50f8491912ed
MBearo <295290708@qq.com>
2020-09-27 14:37:30 +0800

    UPG:226

---
+++ 226.翻转二叉树.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=javascript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (!root) return null
+  invertTree(root.left)
+  invertTree(root.right)
+  const val = root.right
+  root.right = root.left
+  root.left = val
+  return root
+};
+// @lc code=end
+
