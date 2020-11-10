5e44d297af3be7c29af8125f48b18a80a157e11a
MBearo <295290708@qq.com>
2020-11-10 10:02:15 +0800

    UPG:130，服了，上个横列弄反了

--- 130.被围绕的区域.js
+++ 130.被围绕的区域.js
@@ -11,22 +11,23 @@
  */
 // 反其道而行之，找被包围的，那就反着找从边界开始的 O，并且和他相邻的所有 O
 var solve = function (board) {
+  if (board.length === 0) return board
   const rows = board.length;
   const cols = board[0].length
   // 两溜
-  for (let i = 0; i < board.length; i++) {
+  for (let i = 0; i < rows; i++) {
     dfs(i, 0)
-    dfs(i, board[i].length - 1)
+    dfs(i, cols - 1)
   }
   // 两行
-  for (let i = 0; i < board[0].length; i++) {
+  for (let i = 0; i < cols; i++) {
     //console.log(board[0][i]);
     // console.log(board[board.length - 1][i]);
     dfs(0, i)
-    dfs(board.length - 1, i)
+    dfs(rows - 1, i)
   }
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
       if (board[i][j] === 'A') {
         board[i][j] = 'O'
       } else if (board[i][j] === 'O') {
@@ -36,10 +37,11 @@ var solve = function (board) {
   }
 
   function dfs(x, y) {
-    if (x < 0 || y < 0 || x >= cols || y >= rows || board[y][x] === 'X' || board[y][x] === 'A') {
+    if (x < 0 || y < 0 || x > rows - 1 || y > cols - 1 || board[x][y] !== 'O') {
       return
     }
-    board[y][x] = 'A'
+    // console.log(y, x,rows,cols);
+    board[x][y] = 'A'
     dfs(x + 1, y)
     dfs(x - 1, y)
     dfs(x, y + 1)
@@ -50,8 +52,14 @@ var solve = function (board) {
 // @lc code=end
 
 console.log(solve([
-  ['X', 'X', 'X', 'X'],
-  ['X', 'O', 'O', 'X'],
-  ['X', 'X', 'O', 'X'],
-  ['X', 'O', 'X', 'X']
+  ["X", "O", "X", "O", "X", "O"],
+  ["O", "X", "O", "X", "O", "X"],
+  ["X", "O", "X", "O", "X", "O"],
+  ["O", "X", "O", "X", "O", "X"]
 ]));
+// [
+//   ["X","O","X","O","X","O"],
+//   ["O","X","X","X","X","X"],
+//   ["X","X","X","X","X","O"],
+//   ["O","X","O","X","O","X"]
+// ]
