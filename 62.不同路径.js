e3b0948981f5471191b256100a84a19d73867500
MBearo <295290708@qq.com>
2020-12-09 12:30:50 +0800

    UPG: 62

---
+++ 62.不同路径.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=javascript
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  const array = Array(m).fill([])
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === 0 || j === 0) {
+        array[i][j] = 1
+      } else {
+        array[i][j] = array[i - 1][j] + array[i][j - 1]
+      }
+    }
+  }
+  return array[m - 1][n - 1]
+};
+// @lc code=end
+
+console.log(uniquePaths(7,3));
