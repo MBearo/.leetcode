8079612a4ec5c2db44afeefd771bc3677cfda9ed
wuweiqi <wuweiqi@baidu.com>
2021-06-16 20:41:19 +0800

    UPG: 56

--- 56.合并区间.js
+++ 56.合并区间.js
@@ -9,8 +9,19 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function(intervals) {
-
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0])
+  let res = [intervals[0]]
+  for (let i = 1; i < intervals.length; i++) {
+    let interval = intervals[i]
+    let resInterval = res[res.length - 1];
+    if (interval[0] <= resInterval[1]) {
+      resInterval[1] = Math.max(resInterval[1], interval[1]);
+    } else if (interval[0] > resInterval[1]) {
+      res.push(interval)
+    }
+  }
+  return res
 };
 // @lc code=end
 
